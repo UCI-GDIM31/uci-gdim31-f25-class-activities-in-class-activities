45f50cd0d4f7c56397d7711ab46b7b9ca1ea0a31
R3stl3ssDr3ams <solorio.42707@gmail.com>
2025-10-27 19:41:06 -0700

    Update CatW5.cs

--- In Class Activities/Assets/W5/Scripts/CatW5.cs
+++ In Class Activities/Assets/W5/Scripts/CatW5.cs
@@ -34,7 +34,7 @@ public class CatW5 : MonoBehaviour
         //
         if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
         {
-            Vector3 forward = Vector3.up;
+            transform.forward = Vector3.up;
         }
         // STEP 2
         // After Step 1 is working, add more code to make it possible to flip
