725238808d7412484955813dc2057636b231c406
R3stl3ssDr3ams <solorio.42707@gmail.com>
2025-10-27 19:22:12 -0700

    Update DeerW5.cs

--- In Class Activities/Assets/W5/Scripts/DeerW5.cs
+++ In Class Activities/Assets/W5/Scripts/DeerW5.cs
@@ -1,6 +1,20 @@
 using UnityEngine;
 using UnityEngine.AI;
 
+public class DeerW5 : MonoBehaviour
+{ 
+    [SerializeField] private float _moveSpeed = 1.0f;
+    [SerializeField] private float _turnSpeed = 1.0f;
+    [SerializeField] private Transform _path;
+
+
+    private void Start()
+    {
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        agent.SetDestination(_path.position);
+    }
+}
+
 // Write your DeerW5 class in here :)
 // Hint: if you don't remember what a class is supposed to look like,
 //      maybe check out CatW5...
